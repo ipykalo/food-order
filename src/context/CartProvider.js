36dc04889f183ea3dc764735ccdf705de4fcb78e
Ivan <ipyka>
2021-12-17 15:37:22 +0200

    small refactoring

--- src/context/CartProvider.js
+++ src/context/CartProvider.js
@@ -1,18 +1,18 @@
 import { useReducer } from "react";
 import CratContext from "./cart";
 
-const reducer = (state, action) => {
-  if (action.type === 'ADD') {
-    return {
-      items: state.items.concat(action.item),
-      total: +(state.total + action.item.amount * action.item.price).toFixed(2)
-    }
-  } else if (action.type === "REMOVE") {
-    //TODO implemet
-  }
-}
-
 const CartProvider = props => {
+  const reducer = (state, action) => {
+    if (action.type === 'ADD') {
+      return {
+        items: state.items.concat(action.item),
+        total: +(state.total + action.item.amount * action.item.price).toFixed(2)
+      }
+    } else if (action.type === "REMOVE") {
+      //TODO implemet
+    }
+  }
+
   const [ctxState, dispatch] = useReducer(reducer, {
     items: [],
     total: 0
