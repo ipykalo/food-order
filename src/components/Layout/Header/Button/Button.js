57a52f9f481b623227d7a4ee11f9c0cf248bab20
Ivan <ipyka>
2021-12-20 13:33:48 +0200

    added animation

--- src/components/Layout/Header/Button/Button.js
+++ src/components/Layout/Header/Button/Button.js
@@ -1,13 +1,21 @@
-import { useContext } from "react";
+import { useContext, useEffect, useState } from "react";
 import Icon from "./Icon/Icon";
 import classes from "./Button.module.css";
 import CratContext from "../../../../context/cart";
 
 const Button = props => {
+  const [isBump, setIsBump] = useState(false);
   const cart = useContext(CratContext);
 
+  useEffect(() => {
+    setIsBump(true);
+    const id = setTimeout(() => setIsBump(false), 300);
+
+    return () => clearTimeout(id);
+  }, [cart.items]);
+
   return (
-    <button className={classes.button} onClick={props.onClickBtn}>
+    <button className={`${classes.button} ${isBump ? classes.bump : ''}`} onClick={props.onClickBtn}>
       <span className={classes.icon}><Icon /></span>
       <span>Cart</span>
       <span className={classes.badge}>{cart.total}</span>
