5db6767038715ce2afe66d9b2a83035f089d71fc
Ivan <ipyka>
2021-12-22 21:03:13 +0200

    fetch list of meals from the server

--- src/components/Meals/List/List.js
+++ src/components/Meals/List/List.js
@@ -2,35 +2,8 @@ import classes from "./List.module.css";
 import Wrapper from "../../UI/Wrapper/Wrapper";
 import Item from "./Item/Item";
 
-const DUMMY_MEALS = [
-  {
-    id: 'm1',
-    name: 'Sushi',
-    description: 'Finest fish and veggies',
-    price: 22.99,
-  },
-  {
-    id: 'm2',
-    name: 'Schnitzel',
-    description: 'A german specialty!',
-    price: 16.5,
-  },
-  {
-    id: 'm3',
-    name: 'Barbecue Burger',
-    description: 'American, raw, meaty',
-    price: 12.99,
-  },
-  {
-    id: 'm4',
-    name: 'Green Bowl',
-    description: 'Healthy...and green...',
-    price: 18.99,
-  },
-];
-
-const List = () => {
-  const list = DUMMY_MEALS.map(item => (
+const List = props => {
+  const list = props.list.map(item => (
     <Item
       key={item.id}
       id={item.id}
--- src/components/Meals/Meals.js
+++ src/components/Meals/Meals.js
@@ -1,11 +1,20 @@
 import Summary from "./Summary/Summary";
 import List from "./List/List";
+import { useEffect, useState } from "react";
 
 const Meals = () => {
+  const [meals, setMeals] = useState([]);
+
+  useEffect(() => {
+    fetch('http://localhost:4000/meals')
+      .then(resp => resp.json())
+      .then(meals => setMeals(meals));
+  }, []);
+
   return (
     <>
       <Summary />
-      <List />
+      <List list={meals} />
     </>
   );
 }
