ab66b1ea9f3866cc187bd81b2018f1718f16bb15
Ivan <ipyka>
2021-12-28 11:18:31 +0200

    removed dirty code

--- src/store/index.js
+++ src/store/index.js
@@ -1,11 +1,8 @@
 import { configureStore } from "@reduxjs/toolkit";
 import tokenReducer from "../reducers/token";
-import createStore from "react-redux"
 
 export default configureStore({
   reducer: {
     token: tokenReducer
   }
 });
-
-//export default createStore({})
