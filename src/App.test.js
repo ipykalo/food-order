ee507cd84955b072cadf9ea480b4737d4b00ae24
Ivan <ipyka>
2022-01-05 17:54:58 +0200

    added test

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,23 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
+import { Provider } from 'react-redux'
+import configureStore from 'redux-mock-store'
+
 test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const initialState = {
+    token: {
+      tokenData: {
+        token: 'sfssssdsdss',
+        expiresAt: 0,
+        timestamp: new Date()
+      }
+    }
+  }
+  const mockStore = configureStore();
+  const store = mockStore(initialState);
+
+  render(<Provider store={store}><App /></Provider>)
+  const linkElement = screen.getByText(/Login/i);
   expect(linkElement).toBeInTheDocument();
 });
